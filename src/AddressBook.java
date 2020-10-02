5dc5e4b3ff30bcf5deca86513246a953fdeea9c9
Thomas Ross <thomasross@cmail.carleton.ca>
2020-10-01 23:25:34 -0400

    Add comments for addBuddy and removeBuddy

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -15,12 +15,24 @@ public class AddressBook {
         addressBook.removeBuddy(0);
     }
 
+    /**
+     * Add a buddy to the buddies list
+     *
+     * @param buddy The buddy to add
+     */
     public void addBuddy(BuddyInfo buddy) {
         if (buddy != null) {
             this.buddies.add(buddy);
         }
     }
 
+    /**
+     * Remove a buddy from the buddies list
+     *
+     * @param index The index of the buddy to remove
+     *
+     * @return The removed buddy
+     */
     public BuddyInfo removeBuddy(int index) {
         if (index >= 0 && index < this.buddies.size()) {
             return this.buddies.remove(index);
